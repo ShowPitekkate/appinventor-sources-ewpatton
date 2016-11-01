e96f399f1972855d7230df361115d36fdf9b1068
dengxinyue0420 <dxy0420@mit.edu>
2016-10-31 22:34:55 -0400

    add uuid

--- appinventor/blocklyeditor/src/collaboration.js
+++ appinventor/blocklyeditor/src/collaboration.js
@@ -15,32 +15,31 @@ goog.require("socket.io");
 Blockly.Collaboration = function(workspace, channel){
     this.workspace = workspace;
     this.channel = channel;
+    var uuid = generateUUID();
 
     var socket = io.connect("http://localhost:3000", {autoConnect: true});
-    console.log("new collaboration established, channel is "+channel+" user is "+this.user);
+    console.log("new collaboration established, channel is "+channel+" user is "+uuid);
     socket.emit("channel", channel);
 
     socket.on(this.channel, function(msg){
         var msgJSON = JSON.parse(msg);
         var userFrom = msgJSON["user"];
-        //var user = document.getElementById("userEmail").children[1].textContent;
-        //console.log("User"+ user +"receive new events from "+userFrom);
+        console.log("User"+ uuid +"receive new events from "+userFrom);
         console.log(msgJSON["event"]);
-        //if(userFrom != this.user){
+        if(userFrom != uuid){
              var newEvent = Blockly.Events.fromJson(msgJSON["event"], workspace);
              Blockly.Events.disable();
              newEvent.run(true);
              Blockly.Events.enable();
-        //}
+        }
     });
 
     this.workspace.addChangeListener(function(event){
         if(event.type==Blockly.Events.UI){
             return;
         }
-        //var user = document.getElementById("userEmail").children[1].textContent;
         var msg = {
-            //"user" : user,
+            "user" : uuid,
             "event" : event.toJson()
         }
         var eventJson = JSON.stringify(msg);
@@ -49,4 +48,10 @@ Blockly.Collaboration = function(workspace, channel){
     });
 }
 
+function generateUUID(){
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
 
